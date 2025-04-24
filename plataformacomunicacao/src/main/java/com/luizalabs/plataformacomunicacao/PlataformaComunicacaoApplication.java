f33181f428ac351d6084db5b61b4acfd071720d2
Renato Viturino <reviturino@gmail.com>
2025-04-24 18:34:53 -0300

    primeiro commit  criação do projeto

---
+++ plataformacomunicacao/src/main/java/com/luizalabs/plataformacomunicacao/PlataformaComunicacaoApplication.java
@@ -0,0 +1,13 @@
+package com.luizalabs.plataformacomunicacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaComunicacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlataformaComunicacaoApplication.class, args);
+	}
+
+}
---
+++ plataformacomunicacao/src/test/java/com/luizalabs/plataformacomunicacao/PlataformaComunicacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizalabs.plataformacomunicacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformaComunicacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
