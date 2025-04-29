8edb6ea0b338a632291b24cd6e66463e3df5301a
Renato Viturino <renatoviturino@MacBook-Pro-de-MacBook.local>
2025-04-28 22:36:57 -0300

    adicao do controller e arquivo gitignore

---
+++ plataformacomunicacao/src/main/java/com/luizalabs/plataformacomunicacao/controllers/AgendamentoController.java
@@ -0,0 +1,35 @@
+package com.luizalabs.plataformacomunicacao.controllers;
+
+import com.luizalabs.plataformacomunicacao.model.Agendamento;
+import com.luizalabs.plataformacomunicacao.model.AgendamentoRequest;
+import com.luizalabs.plataformacomunicacao.service.AgendamentoRepository;
+import com.luizalabs.plataformacomunicacao.tipo.StatusComunicacao;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/agendamentos")
+@RequiredArgsConstructor
+public class AgendamentoController {
+
+    private final AgendamentoRepository repository;
+
+
+    @PostMapping
+    public ResponseEntity<Agendamento> agendarComunicacao(@RequestBody AgendamentoRequest request) {
+        Agendamento agendamento = Agendamento.builder()
+                .dataHoraEnvio(request.getDataHoraEnvio())
+                .destinatario(request.getDestinatario())
+                .mensagem(request.getMensagem())
+                .tipo(request.getTipo())
+                .status(StatusComunicacao.AGENDADO)
+                .build();
+
+        Agendamento salvo = repository.save(agendamento);
+        return ResponseEntity.ok(salvo);
+    }
+}
